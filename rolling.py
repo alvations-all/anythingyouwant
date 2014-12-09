7721256f043cd1e0e4925d59a139433b42cdbafe
alvas <alvas@ubi>
2014-12-09 09:59:04 +0100

    still testing

--- rolling.py
+++ rolling.py
@@ -76,10 +76,9 @@ def build_word_vector(n=0):
     sentences = []
     with io.open(corpus_name, 'r', encoding='utf8') as fin:
         for line in fin:
-            if '\t' not in line or not line.strip().endswith('.'):
-                continue
-            else:
+            if line.strip().endswith('.'):
                 sentences.append(list(tokenize(line)))
+                
     for s in sentences:
         print s
     #model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=2)
