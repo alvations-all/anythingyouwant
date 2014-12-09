5a0eadbd76f9eadd80ced149ab9d1c85f8aaa5b7
alvas <alvas@ubi>
2014-12-09 10:17:01 +0100

    added script for training vectors

---
+++ deeping.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+python rolling 0 1
+python rolling 0 5
+
+python rolling 1 1
+python rolling 1 5
+
+python rolling 2 1
+python rolling 2 5
+
+python rolling 3 1
+python rolling 3 5
+
+python rolling 4 1
+python rolling 4 5
+
+python rolling 5 1
+python rolling 5 5
+
+python rolling 6 1
+python rolling 6 5
+
+python rolling 7 1
+python rolling 7 5
