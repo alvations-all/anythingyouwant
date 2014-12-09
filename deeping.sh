2cde2422db032dbe60d30a1ad8c1fa914128a50f
alvas <alvas@ubi>
2014-12-09 10:23:22 +0100

    added .py

--- deeping.sh
+++ deeping.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-nohup python rolling 0 1 &
-nohup python rolling 1 1 &
-nohup python rolling 2 1 &
-nohup python rolling 3 1 &
-nohup python rolling 4 1 &
-nohup python rolling 5 1 &
-nohup python rolling 6 1 &
-nohup python rolling 7 1 &
+nohup python rolling.py 0 1 &
+nohup python rolling.py 1 1 &
+nohup python rolling.py 2 1 &
+nohup python rolling.py 3 1 &
+nohup python rolling.py 4 1 &
+nohup python rolling.py 5 1 &
+nohup python rolling.py 6 1 &
+nohup python rolling.py 7 1 &
