6656e5f2380e1b69923bc6c88e2ede5a7cd8dae6
alvas <alvas@ubi>
2014-12-09 16:42:04 +0100

    let's build matrices

--- deeping.sh
+++ deeping.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-nohup python rolling.py 0 1 &
-nohup python rolling.py 1 1 &
-nohup python rolling.py 2 1 &
-nohup python rolling.py 3 1 &
-nohup python rolling.py 4 1 &
-nohup python rolling.py 5 1 &
-nohup python rolling.py 6 1 &
-nohup python rolling.py 7 1 &
+nohup python rolling.py 0 &
+nohup python rolling.py 1 &
+nohup python rolling.py 2 &
+nohup python rolling.py 3 &
+nohup python rolling.py 4 &
+nohup python rolling.py 5 &
+nohup python rolling.py 6 &
+nohup python rolling.py 7 &
