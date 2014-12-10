0cd15a53e2ef6471f3bbd328b34bde94f62f5d5e
alvas <alvas@ubi>
2014-12-10 10:56:06 +0100

    fixed the clustering scripts

--- scipy_clustering.py
+++ scipy_clustering.py
@@ -3,26 +3,28 @@
 from itertools import product
 
 import numpy as np
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pylab as plt
 
 from texeval import TexEval2015
 
+nd = 2
 texeval_corpus = TexEval2015()
-
+sbcs = texeval_corpus.test_subcorpora
+sbc = sbcs[nd]
+terms = [i[1] for i in texeval_corpus.terms('test', sbc)]
+numdomain = [i for i in range(1,8)]
 
 methods = 'single complete average weighted'.split()
 criterion = 'inconsistent distance maxclust'.split()
 numclust = sorted([int(len(terms)/float(i)) for i in range(1,4)])
 
-sbcs = texeval_corpus.test_subcorpora
-numdomain = [i for i in range(1,8)] 
-
-for me, cr, nc, nd  in product(methods, criterion, numclust, numdomain):
-    sbc = sbcs[nd] 
+for me, cr, nc  in product(methods, criterion, numclust):
     terms = [i[1] for i in texeval_corpus.terms('test', sbc)]
     matrix = np.loadtxt(sbc+'.matrix')
     x = linkage(matrix, method=me)
     y = dendrogram(x)
     treename = "{}.{}.{}.png".format(me, cr, nc)
-    plt.savefig(treename)
+    plt.savefig('dendrograms/'+treename)
+    z = fcluster(x,nc,cr)
+    
