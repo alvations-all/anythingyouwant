0961428ed78c95959876c773885bc2cb43b4f975
alvas <alvas@ubi>
2014-12-10 17:42:27 +0100

    ...

--- rolling2.py
+++ rolling2.py
@@ -97,13 +97,13 @@ def build_word_vector(n=0, mincount=1):
                      min_count=mincount, workers=2, iter=100)
     model.save(corpus_name+'.10epochs.singletok.min'+str(mincount)+'.deep')
 
-def test_vector(n=3, mincount=1):
+def test_vector(n=2, mincount=1):
     sbcs = texeval_corpus.test_subcorpora
     sbc = sbcs[n]
     fname = 'WIKI_'+sbc+'.10epochs.singletok.min'+str(mincount)+'.deep'
     model = Word2Vec.load(fname)
     
-    print model.most_similar(positive=['orange', 'is_a'])
+    print model.most_similar(positive=['orange', 'is', 'a'])
 
 test_vector()
 
