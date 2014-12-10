9e51dc89a131d06e85cd633d5e1e24cd079a86c0
alvas <alvas@ubi>
2014-12-10 15:15:55 +0100

    added sklearn

--- sklearn_clustering.py
+++ sklearn_clustering.py
@@ -7,38 +7,66 @@ import matplotlib.pyplot as plt
 import numpy as np
 from texeval import TexEval2015
 
+import random
+
+random.seed(3)
+
 texeval_corpus = TexEval2015()
 
 n=2
 sbcs = texeval_corpus.test_subcorpora
 sbc = sbcs[n] 
-terms = [i[1] for i in texeval_corpus.terms('test', sbc)]
+terms = [row[1] for row in texeval_corpus.terms('test', sbc)]
 
 matrix = np.loadtxt(sbc+'.matrix')
 
 n_clusters = int(len(terms)/float(3))
 model = AgglomerativeClustering(n_clusters=n_clusters,
-                                linkage="average", affinity="cosine")
+                                linkage="ward", affinity="euclidean")
 model.fit(matrix)
 
-print n_clusters
+print n_clusters, len(terms)
+'''
+print model.children_[500]
+print model.children_[520]
+print model.children_[1520]
+'''
+l = len(terms)
+'''
+for row in model.children_:
+    if int(row[0]) > l or int(row[1]) > l:
+        continue
+    else:
+            
+        try:
+            t1 = terms[row[0]]
+            t2 = terms[row[1]]
+            print row, t1, " ||| ", t2
+        except:
+            continue
+'''
 
-#for i in model.children_:
-#    print i
 
-##print n_clusters, len(terms), min(model.labels_), max(model.labels_)
+print n_clusters, len(terms), min(model.labels_), max(model.labels_)
+
 
 # Get clusters.
 for term, clusterid in zip(terms, model.labels_):
-    print term, clusterid
+    print clusterid, term
 
-print terms
+
+
+
+
+
+
+
 
 
 '''
 for l in np.arange(model.n_clusters):
-    for i in model.labels_ == l:
-        print i
+    for row in model.labels_ == l:
+        print row
     #print matrix[model.labels_ == l].T
     break
 ''' 
