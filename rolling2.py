a69d39399e01ac0c6c767ef4daa449bb3f83f430
alvas <alvas@ubi>
2014-12-10 17:55:11 +0100

    burning the server with 100 epochs, i need more servers CPU....

--- rolling2.py
+++ rolling2.py
@@ -95,8 +95,8 @@ def build_word_vector(n=0, mincount=1):
                     line = line.replace(current_term, depunct_term)
                     sentences.append(list(tokenize(line)))
     model = Word2Vec(sentences, size=100, window=5, 
-                     min_count=mincount, workers=2, iter=10)
-    model.save(corpus_name+'.10epochs.singletok.min'+str(mincount)+'.deep')
+                     min_count=mincount, workers=2, iter=100)
+    model.save(corpus_name+'.100epochs.singletok.min'+str(mincount)+'.deep')
     
                 
 def test_vector(n=2, mincount=1):
